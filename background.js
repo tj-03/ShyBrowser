12f1d133f7aa165149cefc3ef2e5b6b2635eb248
tj-03 <90046683+tj-03@users.noreply.github.com>
2021-09-26 12:05:30 -0400

    Update background.js

--- background.js
+++ background.js
@@ -9,20 +9,34 @@ chrome.runtime.onInstalled.addListener(() => {
 
 async function getTabs(){
   let queryOptions = {};
+  let [currentTab] = await chrome.tabs.query({active:true,currentWindow:true});
+
   let tabs = await chrome.tabs.query(queryOptions);
+
+ 
   return tabs;
 }
 
 async function removeTab(){
   let tabs = await getTabs();
   console.log(tabs.length);
-  chrome.tabs.remove(tabs[tabs.length-2].id);
+
+  chrome.tabs.remove(tabs[Math.floor(Math.random()*tabs.length)].id);
   
 }
 
 chrome.runtime.openOptionsPage(()=>{})
+
 chrome.runtime.onMessage.addListener(async function(msg,sender,callback){
     await removeTab();
+});
+
+chrome.runtime.onConnect.addListener(function(port) {
+  if (port.name === "options") {
+      port.onDisconnect.addListener(function() {
+        chrome.runtime.openOptionsPage(()=>{})
+      });
+  }
 });
 
 
